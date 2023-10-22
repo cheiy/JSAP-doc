853b738d1321af8afaeb11f9e2220940951608e3
cheiy <gschege@gmail.com>
2023-10-22 19:12:54 +0000

    Routes

--- app/home/views.py
+++ app/home/views.py
@@ -11,4 +11,24 @@ def homepage():
     Render the homepage template on the / route
     """
     return render_template('home/landingpage.html', title="Welcome")
+@home.route('/registration.html')
+def registration():
+    """
+    Render the registration form for doctors
+    """
+    return render_template('home/registration.html', title='Are you a doctor? Enter your details here')
+
+@home.route('/appointment.html')
+def appointment():
+    """
+    Render the appointment form for patients
+    """
+    return render_template('home/appointment.html', title="Schedule an appointment")
+
+@home.route('/login.html')
+def login():
+    """
+    Render the login form
+    """
+    return render_template('home/login.html', title="Login")
 
